1e98c9e6840bc9274cd29116df7cfd358f24166a
Ignacio Cinalli <nachocinalli@gmail.com>
2021-11-18 12:38:47 -0300

    fix model path

--- js/GroupItemsModel.js
+++ js/GroupItemsModel.js
@@ -1,4 +1,4 @@
-import ItemsComponentModel from 'core/js/models/ItemsComponentModel';
+import ItemsComponentModel from 'core/js/models/itemsComponentModel';
 
 export default class GroupItemsModel extends ItemsComponentModel {
   defaults() {
